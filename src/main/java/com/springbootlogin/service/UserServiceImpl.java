bc38aedf2324f8ff57cc7dad1aed0ba679bbd025
Your Name <dineshvignesh001@gmail.com>
2019-09-26 16:34:18 +0530

    first commit

---
+++ src/main/java/com/springbootlogin/service/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.springbootlogin.service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.springbootlogin.model.Role;
+import com.springbootlogin.model.User;
+import com.springbootlogin.repository.RoleRespository;
+import com.springbootlogin.repository.UserRepository;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+ 
+ @Autowired
+ private UserRepository userRepository;
+ 
+ @Autowired
+ private RoleRespository roleRespository;
+ 
+ @Autowired
+ private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+ @Override
+ public User findUserByEmail(String email) {
+  return userRepository.findByEmail(email);
+ }
+
+ @Override
+ public void saveUser(User user) {
+  user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+  user.setActive(1);
+  Role userRole = roleRespository.findByRole("ADMIN");
+  user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+  userRepository.save(user);
+ }
+
+}
